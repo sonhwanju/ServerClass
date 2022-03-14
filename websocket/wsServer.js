762290d9ba3836e673d6245d2c4819deb401a773
hwanjuson <hwanjuson@gmail.com>
2022-03-14 18:03:32 +0900

    2022 03 14 서버수업

---
+++ websocket/wsServer.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const wsService = new WebSocket.Server({port:8080}, () => {
+    console.log("웹 소켓이 8080에서 구동중")
+});
+
+wsService.on("connection", socket => {
+    console.log("소켓 연결");
+
+    socket.on("close", () => {
+        console.log("소켓 연결 해제");
+    });
+
+    socket.on("message", msg => {
+        const data = JSON.parse(msg);
+        console.log(data);
+        socket.send(JSON.stringify({data}));        
+    });
+});
+
+wsService.on("listening", () => {
+    console.log("server listening on port 8080");
+});
