1f4d8426405bf5882318701bf248776070df5423
hwanjuson <hwanjuson@gmail.com>
2022-03-21 21:02:04 +0900

    아까 서버시간에 한 코드

--- websocket/chatServer.js
+++ websocket/chatServer.js
@@ -3,29 +3,30 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const port =  process.env.PORT || 8080;
-
 const server = http.createServer(express);
-
 const wsService = new WebSocket.Server({server});
 
 let socketIdx = 1;
 
-wsService.on("connection", socket => {
-    socket.id = socketIdx++;
+wsService.on("connection", (socket,req) => {
     console.log("소켓 연결");
+    socket.id = socketIdx++;
 
     socket.on("close", () => {
         console.log("소켓 연결 해제");
     });
 
+    const ip = socket._socket.remoteAddress || req.socket.remoteAddress;
+
     socket.on("message", msg => {
         wsService.clients.forEach(soc => {
             if(soc.id == socket.id) return;
-            soc.send(msg.toString());
+            soc.send(ip + " : " + msg.toString());
         });      
     });
 });
 
-wsService.on("listening", () => {
-    console.log(`server listening on port ${port}`);
+server.listen(port, ()=>{
+    console.log(`Server is listening on ${port}`);
 });
+
